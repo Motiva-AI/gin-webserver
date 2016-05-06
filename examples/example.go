8e0855d2c42d645fba345a98a761db52d3ae5761
x <lostinblue@tuta.io>
2016-05-06 15:01:40 +0200

    The example does not work on my computer but the original code did.

--- examples/example.go
+++ examples/example.go
@@ -1,46 +1,53 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/glibs/gin-webserver"
 )
 
 func main() {
-	server := InitializeServer(":80")
+	host := "127.0.0.1:81"
+	server := InitializeServer(host)
 	server.Start()
+	log.Println("Gin web server started on " + host)
+	for {
+		// Hold program open to test web server
+	}
 	// After the server is not useful
-	server.Stop()
+	// server.Stop()
 }
 
 func InitializeServer(host string) (server *network.WebServer) {
-	gin.SetMode(gin.ReleaseMode)
-	engine := gin.New()
+	//gin.SetMode(gin.ReleaseMode)
+	engine := gin.Default()
 	engine.LoadHTMLGlob("./templates/*")
 	engine.Static("/public/css/", "./public/css")
 	engine.Static("/public/js/", "./public/js/")
 	engine.Static("/public/fonts/", "./public/fonts/")
 	engine.Static("/public/img/", "./public/img/")
 
-	server.GET("/", GetIndex)
-	server.GET("/about", GetAbout)
-	server.GET("/contact", GetContact)
+	engine.GET("/", getIndex)
+	engine.GET("/about", getAbout)
+	engine.GET("/contact", getContact)
 
 	return network.InitializeWebServer(engine, host)
 }
 
-func GetIndex(c *gin.Context) {
+func getIndex(c *gin.Context) {
 	c.HTML(200, "index.html", gin.H{
 		"variableName": "value",
 	})
 }
 
-func GetAbout(c *gin.Context) {
+func getAbout(c *gin.Context) {
 	c.HTML(200, "about.html", gin.H{
 		"variableName": "value",
 	})
 }
 
-func GetContract(c *gin.Context) {
+func getContact(c *gin.Context) {
 	c.HTML(200, "contact.html", gin.H{
 		"variableName": "value",
 	})
